6d637d0393332a728e996eeb1fb6dd1e661686fc
Dinelle Zipser <dinellezipser@gmail.com>
2022-04-15 09:43:41 -0700

    firebase config added

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCGvDZPSf2eodkVG8SUXorPwAUR62k7wXQ",
+    authDomain: "mymoney-f63cf.firebaseapp.com",
+    projectId: "mymoney-f63cf",
+    storageBucket: "mymoney-f63cf.appspot.com",
+    messagingSenderId: "298630537300",
+    appId: "1:298630537300:web:4fac49bb35828021e04dc1"
+  };
+
+  //init firebase
+  firebase.initializeApp(firebaseConfig);
+
+  //init service
+  const projectFirestore = firebase.firestore()
+
+  export { projectFirestore }
